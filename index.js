dd7588898fd6beb92d257a81c457ac7f840c4347
Kleber Sousa <49425412+kleber-smartdev@users.noreply.github.com>
2022-07-13 06:35:34 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function setThemePreference() {
   let d = new Date();
   /*
-  * The getHours() method returns the hour (from 0 to 23) of the specified date and time.
-  * Early mornig = 0 - 6
-  * Morning = 6 - 12
-  * Evening = 12 - 18
-  * Night = 18 - 23
+  * The getHours() method returns the hour (from 0:00 to 23:59) of the specified date and time.
+  * Early morning = 0:00 - 6:59
+  * Morning = 7:00 - 12:00
+  * Afternoon = 12:01 - 17:59
+  * Night = 18:00 - 23:59
   */
-  let currentHour = d.getHours();  
-  
-  if(currentHour >= 19 || currentHour <= 6) {
+  let currentHour = d.getHours();
+
+  if ( currentHour >= 19 || currentHour <= 7 ) {
     document.body.setAttribute("data-theme", "dark_theme") 
-  }else {
+  } else {
     document.body.setAttribute("data-theme", "light_theme") 
   }
 }
